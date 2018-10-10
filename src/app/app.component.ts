371b1849fad9669f380eaba3c61d22564b7db75f
Zhicheng Wang <zcwang@thoughtworks.com>
2018-10-10 09:05:24 +0800

    feat: IFrame 通讯

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { isUndefined } from 'util';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-applet-a';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    if (!isUndefined(window)) {
+      window.addEventListener('message', (msg: MessageEvent) => {
+        const data = msg.data as { channel: string, message: string };
+        if (data.channel === 'mf-master') {
+          msg.source.postMessage({ channel: 'mf-slave', message: 'Hi' }, '*');
+          alert(`${data.message} @ ng-applet-a`);
+        }
+      });
+    }
+  }
 }
